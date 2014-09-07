8951b5f3f26b6138f933c0f8ed6b2ad99b88aaa5
li-on <li-on@wp.pl>
2014-09-07 20:00:59 +0200

    tłumaczenie na język polski - bez tutoriala i pomocy

---
+++ BuildPolski.bat
@@ -0,0 +1,2 @@
+bin\Tools-x64.exe /build Package.rsc:resource /pathres ../polski /pathdat ../polski/bin
+bin\Tools-x64.exe /mod Package.rsc:PolishTranslation /pathres ../polski /pathdat ../polski/bin
